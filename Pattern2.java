491c50c0e8441fd55fadbc7b0c5d1d8f4c707064
Saikumaroddi-spec <saikumaroddi5@gmail.com>
2025-01-25 15:18:20 +0530

    no of alpha

--- Pattern2.java
+++ Pattern2.java
@@ -1,16 +1,17 @@
 
 public class Pattern2 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		for(int r=1;r<=5;r++){
-			for(int c=5,p=1;p<=r;c--,p++){
-			System.out.print(c+" ");
-		}
-		System.out.println( );
-					
+		for(int r=1;r<=5;r++) {// r--1
+			for(int c=5;c>=r;c--) {// 5 4 3 2 1
+				System.out.print(c+" ");
+			}
+			System.out.println();
 		}
 		
+		
+		
+		
+		
 	}
+
 }
--- README.md
+++
@@ -1,1 +0,0 @@
-"# javacrtday5" 
